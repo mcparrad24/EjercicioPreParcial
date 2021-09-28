74ebaa07ed8fc59cfc3b665539b0cde066eb1cda
Camila Parra <mc.parrad@uniandes.edu.co>
2021-09-28 12:16:58 -0500

    Se agregan datos del form

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ fetch("archivo.json").then(res=>res.json()).then(res=>{
     llenarTabla(datos);
 })
 
-
 const thead1 = document.getElementById("head1");
 const tbody1 = document.getElementById("body1");
-const forms = document.forms;
+const form = document.getElementById("form");
+
 const filaPrincipal = document.createElement("tr");
 const col1Principal = document.createElement("th");
 const col2Principal = document.createElement("th");
@@ -18,6 +18,11 @@ col2Principal.textContent = "First Name";
 col3Principal.textContent = "Email";
 col4Principal.textContent = "Photo";
 
+const col1Form = document.createElement("td");
+const col2Form = document.createElement("td");
+const col3Form = document.createElement("td");
+const col4Form = document.createElement("td");
+
 let data = [];
 let dataSorted = [];
 
@@ -25,6 +30,8 @@ col1Principal.onclick = ordCol;
 col2Principal.onclick = ordCol;
 col3Principal.onclick = ordCol;
 col4Principal.onclick = ordCol;
+
+form.onsubmit = agregarForm;
 
 function llenarTabla(json){
     filaPrincipal.appendChild(col1Principal);
@@ -75,6 +82,8 @@ function ordCol(element){
 
     thead1.appendChild(filaPrincipal);
 
+    console.log(data);
+
     let columna = 4;
     let elemento = element.target.textContent;
     if (elemento == 'Last Name'){
@@ -108,7 +117,53 @@ function ordCol(element){
         fila.appendChild(col4);
         
         tbody1.appendChild(fila);
+
+        fila.onmouseover = function(){this.style.backgroundColor = "LightCoral";}
+        fila.onmouseout = function(){this.style.backgroundColor = "White";}
     }
+}
+
+function agregarForm(elemento){
+    removeChildNodes(thead1);
+    removeChildNodes(tbody1);
+
+    filaPrincipal.appendChild(col1Principal);
+    filaPrincipal.appendChild(col2Principal);
+    filaPrincipal.appendChild(col3Principal);
+    filaPrincipal.appendChild(col4Principal);
+
+    thead1.appendChild(filaPrincipal);
+
+    let valores = elemento.target.elements;
+
+    let dato = [valores[0].value, valores[1].value, valores[2].value, valores[3].value];
+    data.push(dato);
+    dataSorted.push(dato);
+
+    for(let i = 0; i < data.length; i++){
+        const fila = document.createElement("tr");
+        const col1 = document.createElement("td");
+        const col2 = document.createElement("td");
+        const col3 = document.createElement("td");
+        const col4 = document.createElement("td");
+
+        col1.textContent = data[i][0];
+        col2.textContent = data[i][1];
+        col3.textContent = data[i][2];
+        col4.textContent = data[i][3];
+
+        fila.appendChild(col1);
+        fila.appendChild(col2);
+        fila.appendChild(col3);
+        fila.appendChild(col4);
+        
+        tbody1.appendChild(fila);
+
+        fila.onmouseover = function(){this.style.backgroundColor = "LightCoral";}
+        fila.onmouseout = function(){this.style.backgroundColor = "White";}
+    }
+    elemento.target.reset();
+    return false;
 }
 
 function removeChildNodes(parent){
@@ -117,4 +172,3 @@ function removeChildNodes(parent){
     }
 }
 
-//FORMS: forms[i].elements[i].nodeValue?
